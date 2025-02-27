fbe6b8556ef75526a43a5c76d9047a83ee51b7ec
sherifsani <sanisherif11@gmail.com>
2025-02-27 18:12:31 +0100

    completed USP on desktop

---
+++ app/components/Usp.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Usp = () => {
+    const data = [
+      {
+        img: "/Package.png",
+        head: "faster delivery",
+        desc: "Delivery in 24h",
+      },
+      {
+        img: "/Trophy.png",
+        head: "24 hours return",
+        desc: "100% money back guarantee",
+      },
+      {
+        img: "/CreditCard.png",
+        head: "secure payment",
+        desc: "Your money is safe",
+      },
+      {
+        img: "/Headphones.png",
+        head: "support 24/7",
+        desc: "Live contact/message",
+      },
+    ];
+  return (
+    <div className="flex w-full flex-wrap justify-between mt-8 rounded-md border py-4 md:py-6 ">
+          {data.map(({ img, head, desc }, idx) => (
+              <div key={idx} className={`flex items-center justify-center w-1/4 gap-4 ${
+                  idx !== data.length - 1 ? "md:border-r" : "md:border-r-0"
+              } `}>
+            <img src={img} alt={`${head} icon`} className="w-10 h-10" />
+            <p className="flex flex-col items-start">
+              <span className="text-gray-900 text-sm font-semibold">{head.toUpperCase()}</span>
+              <span className="text-gray-600 text-[12px]">{desc}</span>
+            </p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Usp;
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { HeroGrid } from "./components/HeroGrid";
 import HeroCard from "./components/micro-components/HeroCard";
 import HeroGrids from "./components/HeroGrids";
+import Usp from "./components/Usp";
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-5xl mt-8">
       {/* <HeroGrid /> */}
       <HeroGrids />
+      <Usp/>
     </div>
   );
 }
