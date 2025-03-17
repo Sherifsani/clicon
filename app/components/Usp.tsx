715ceeaec979e8e0793e40d39b863e907f74fe6f
Sherif Sani <sanisherif11@gmail.com>
2025-03-17 17:35:01 +0100

    refactor Usp component layout to use a grid system for improved responsiveness and styling

--- app/components/Usp.tsx
+++ app/components/Usp.tsx
@@ -1,3 +1,4 @@
+import { div } from "motion/react-client";
 import React from "react";
 
 const Usp = () => {
@@ -25,23 +26,27 @@ const Usp = () => {
     },
   ];
   return (
-    <div className="flex flex-col items-start md:flex-row w-full mx-3 md:mx-0 flex-wrap justify-between mt-8 rounded-md border py-4 md:py-6 ">
-      {data.map(({ img, head, desc }, idx) => (
-        <div
-          key={idx}
-          className={`flex items-center justify-center w-full md:w-1/4 gap-4 ${
-            idx !== data.length - 1 ? "md:border-r" : "md:border-r-0"
-          } `}
-        >
-          <img src={img} alt={`${head} icon`} className="w-10 h-10" />
-          <p className="flex flex-col items-start">
-            <span className="text-gray-900 text-sm font-semibold">
-              {head.toUpperCase()}
-            </span>
-            <span className="text-gray-600 text-[12px]">{desc}</span>
-          </p>
-        </div>
-      ))}
+    <div className="mt-8 px-3 md:px-0 w-full">
+      <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-5 md:border rounded-md">
+        {data.map(({ img, head, desc }, idx) => (
+          <div
+            key={idx}
+            className={`flex flex-col md:flex-row items-center justify-center border p-2 gap-4 md:py-5 ${
+              idx !== data.length - 1
+                ? "border-0 md:border-r"
+                : "border-0 md:border-r-0"
+            } `}
+          >
+            <img src={img} alt={`${head} icon`} className="w-10 h-10" />
+            <p className="flex flex-col text-center md:items-start">
+              <span className="text-gray-900 text-sm font-semibold">
+                {head.toUpperCase()}
+              </span>
+              <span className="text-gray-600 text-[12px]">{desc}</span>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
